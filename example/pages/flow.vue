59581d7052679656ef5c0acc278bb7c36dbd6f1d
tianyong90 <412039588@qq.com>
2017-10-28 12:18:58 +0800

    flow

--- example/pages/flow.vue
+++ example/pages/flow.vue
@@ -19,6 +19,16 @@
       <wv-flow-line></wv-flow-line>
       <wv-flow-node title="title">4</wv-flow-node>
     </wv-flow>
+
+    <wv-flow>
+      <wv-flow-node :done="true" title="title">1</wv-flow-node>
+      <wv-flow-line type="process"></wv-flow-line>
+      <wv-flow-node title="title">2</wv-flow-node>
+      <wv-flow-line></wv-flow-line>
+      <wv-flow-node title="title">3</wv-flow-node>
+      <wv-flow-line></wv-flow-line>
+      <wv-flow-node title="title">4</wv-flow-node>
+    </wv-flow>
   </div>
 </template>
 
